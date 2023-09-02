c97261401d26b98e3563b783e090326214686953
AllenWong <allenwong2407@outlook.com>
2023-09-02 16:35:03 +0800

    Bootcamp #6 Icons

--- SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/GradientsBootcamp.swift
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/GradientsBootcamp.swift
@@ -25,7 +25,8 @@ struct GradientsBootcamp: View {
                 AngularGradient(
                     gradient: Gradient(colors: [Color(#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)), Color(#colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha: 1))]),
                     center: .topLeading,
-                    angle: .degrees(180 + 45))
+                    angle: .degrees(180 + 45)
+                )
             )
             .frame(width: 300, height: 200)
     }
