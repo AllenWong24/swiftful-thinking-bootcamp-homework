70b15e9bff645062d13ee0b9b1b93937afdf233b
AllenWong <allenwong2407@outlook.com>
2023-10-05 16:24:25 +0800

    Bootcamp #52 AppStorageBootcamp

---
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/AppStorageBootcamp.swift
@@ -0,0 +1,34 @@
+//
+//  AppStorageBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Allen Wong on 2023/10/5.
+//
+
+import SwiftUI
+
+struct AppStorageBootcamp: View {
+    
+    @AppStorage("name") var currentUserName: String?
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            Text(currentUserName ?? "Add Name Here")
+            
+            if let name = currentUserName {
+                Text(name)
+            }
+            
+            Button("Save".uppercased()) {
+                let name: String = "Emily"
+                currentUserName = name
+            }
+        }
+    }
+}
+
+struct AppStorageBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        AppStorageBootcamp()
+    }
+}
