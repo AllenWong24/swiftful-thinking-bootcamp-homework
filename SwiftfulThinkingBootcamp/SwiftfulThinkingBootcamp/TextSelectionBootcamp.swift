ad2196433fde464e292641ff59378959bf88192a
AllenWong <allenwong2407@outlook.com>
2023-10-06 14:51:47 +0800

    Bootcamp #56 TextSelectionBootcamp

---
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/TextSelectionBootcamp.swift
@@ -0,0 +1,21 @@
+//
+//  TextSelectionBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Allen Wong on 2023/10/6.
+//
+
+import SwiftUI
+
+struct TextSelectionBootcamp: View {
+    var body: some View {
+        Text("Hello, World!")
+            .textSelection(.enabled)
+    }
+}
+
+struct TextSelectionBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        TextSelectionBootcamp()
+    }
+}
