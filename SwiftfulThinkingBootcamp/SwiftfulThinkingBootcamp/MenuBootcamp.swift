91b039bc31c4ca180cecc9118e846f0c5db90095
AllenWong <allenwong2407@outlook.com>
2023-10-06 21:12:49 +0800

    Bootcamp #68 MenuBootcamp

---
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/MenuBootcamp.swift
@@ -0,0 +1,33 @@
+//
+//  MenuBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Allen Wong on 2023/10/6.
+//
+
+import SwiftUI
+
+struct MenuBootcamp: View {
+    var body: some View {
+        Menu("Click me!") {
+            Button("One") {
+                
+            }
+            Button("Two") {
+                
+            }
+            Button("Three") {
+                
+            }
+            Button("Four") {
+                
+            }
+        }
+    }
+}
+
+struct MenuBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuBootcamp()
+    }
+}
